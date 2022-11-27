ecdbec4f6e5f7427af6b7810ccebf43feb37d89d
Kevin <kwsf123@gmail.com>
2022-11-27 04:32:23 -0300

    actualizacion funcional v0.2 (Falta rellenar con la informacion)

--- app/src/main/java/com/ks/guiadedarksoulsiii/MainActivity.java
+++ app/src/main/java/com/ks/guiadedarksoulsiii/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Proximamente...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
